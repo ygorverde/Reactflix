3b3345c36ce41879ae731e6b5c9e300fce0c2c0c
ygorverde <ygorverde572@gmail.com>
2020-07-28 14:34:06 -0300

    Aplicação SPA

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video/index.js'
+import CadastroCategoria from './pages/cadastro/Categoria/index.js'
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+const pagina404 = () => (<div><h1>PÁGINA 404</h1></div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+
+      <Route component={pagina404} exact />
+    </Switch>
+
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
