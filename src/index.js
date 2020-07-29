d4826cca0e0cb41e29283197be70c4d3c2e8269f
ygorverde <ygorverde572@gmail.com>
2020-07-29 13:13:34 -0300

    Remoção warning VideoCardList

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import CadastroVideo from './pages/cadastro/Video/index.js'
-import CadastroCategoria from './pages/cadastro/Categoria/index.js'
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-const pagina404 = () => (<div><h1>PÁGINA 404</h1></div>)
+// Desafio master blaster na descrição
+// Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :)
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-
   <BrowserRouter>
-
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route path="/cadastro/categoria" component={CadastroCategoria} />
-
-      <Route component={pagina404} exact />
+      <Route component={Pagina404} />
     </Switch>
-
   </BrowserRouter>,
-
   document.getElementById('root')
 );
